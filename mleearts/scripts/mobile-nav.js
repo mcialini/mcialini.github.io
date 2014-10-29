c89e4915963a1af519998a684a859901365a7171
Matt Cialini <matt.cialini@gmail.com>
2014-10-28 23:38:26 -0400

    a

--- mleearts/scripts/mobile-nav.js
+++ mleearts/scripts/mobile-nav.js
@@ -1,11 +1,5 @@
 $(document).ready(function(){
-    $("body").on("click touchend", "#mobile-nav li.top #inner-menu.shown", function() {
-        $("#inner-menu").removeClass("shown");
-        $("#inner-menu").addClass("hidden");
-    });
-
-    $("body").on("click touchend", "#mobile-nav li.top #inner-menu.hidden", function() {
-        $("#inner-menu").removeClass("hidden");
-        $("#inner-menu").addClass("shown");
+    $("body").on("click touchend", "#mobile-nav li.top", function() {
+        $("#inner-menu").toggle(250);
     });
 });
