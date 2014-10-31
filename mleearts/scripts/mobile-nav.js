562fcce26ce8a783437e45f9ec821df5a7120949
Matt Cialini <matt.cialini@gmail.com>
2014-10-30 21:25:18 -0400

    a

--- mleearts/scripts/mobile-nav.js
+++ mleearts/scripts/mobile-nav.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     $("body").on("click touchend", "#mobile-nav li.top", function() {
-    	alert("touched");
+    	// alert("touched");
         $("#inner-menu").toggle(250);
+        return false;
     });
 });
