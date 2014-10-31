2c9561e7948bf6cc52e3827748412937ba50433f
Matt Cialini <matt.cialini@gmail.com>
2014-10-30 21:32:30 -0400

    sdsd

--- mleearts/scripts/mobile-nav.js
+++ mleearts/scripts/mobile-nav.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $("body").on("click touchend", "#mobile-nav li.top", function() {
+    $("body").on("click touchend", "#mobile-nav #menu-link", function() {
     	// alert("touched");
         $("#inner-menu").toggle(250);
         return false;
