e67e273a864726d778e967c9250a8ab7a9def3a2
Matt Cialini <matt.cialini@gmail.com>
2015-09-09 21:18:50 -0400

    Testing

--- mleearts2/scripts/mobile-nav.js
+++ mleearts2/scripts/mobile-nav.js
@@ -2,13 +2,15 @@ $(document).ready(function(){
     $("body").on("click touchend", "nav .mobile #menu-link", function(event) {
         event.stopPropagation();
         event.preventDefault();
-        $("#inner-menu").toggle(250, function() {
-            if ($("#inner-menu").is(":visible")) {
-                $("#menu-link").text("CLOSE MENU");
-            } else {
-                $("#menu-link").text("MENU");
-            }            
-        });
+        if (event.handled !== true) {
+            $("#inner-menu").toggle(250, function() {
+                if ($("#inner-menu").is(":visible")) {
+                    $("#menu-link").text("CLOSE MENU");
+                } else {
+                    $("#menu-link").text("MENU");
+                }            
+            });
+        }
         return false;
     });
 });
