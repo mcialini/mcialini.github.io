2124ad24b552c68af03952ddf79d60f68f766d6a
Matt Cialini <matt.cialini@gmail.com>
2015-09-09 21:24:49 -0400

    Testing

--- mleearts2/scripts/mobile-nav.js
+++ mleearts2/scripts/mobile-nav.js
@@ -1,14 +1,15 @@
 $(document).ready(function(){
+    var ready = true;
     $("body").on("click touchend", "nav .mobile #menu-link", function(event) {
-        event.stopPropagation();
-        event.preventDefault();
-        if (event.handled !== true) {
+        if (ready === true) {
+            ready = false;
             $("#inner-menu").toggle(250, function() {
+                ready = true;
                 if ($("#inner-menu").is(":visible")) {
                     $("#menu-link").text("CLOSE MENU");
                 } else {
                     $("#menu-link").text("MENU");
-                }            
+                }         
             });
         }
         return false;
