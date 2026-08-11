7e30427d240f1a82ade0d24d86a5bd1a09d2ffbb
Matt Cialini <matt.cialini@gmail.com>
2026-08-10 20:03:08 -0400

    Remove old website files

--- mleearts.net/scripts/mobile-nav.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $("body").on("click touchend", "#mobile-nav #menu-link", function() {
-    	// alert("touched");
-        $("#inner-menu").toggle(250);
-        return false;
-    });
-});
